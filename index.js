e3928b1fcccdc12cf129049a8d0655a1df4aa05e
SnowBit <dtshah24022006@gmail.com>
2021-12-10 20:28:28 +0530

    Added Roasts

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log(`Roast`)
+
+
+const data = require('./data/data.json'),
+
+    randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min),
+    i = randomInt(0, data.length)
+
+
+    // Get Random Roast
+    roast = () => data[i]
+
+module.exports = {
+    roast
+}
+
+console.log(roast())
